1782eebed790e2b374b2388c4d0554c71209d3dd
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-06-06 14:34:24 -0500

    fixed book model

--- models/books.js
+++ models/books.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose 
 
 // schema:
-const bookSchema = new Schema({
-        title: String,
-        description: String,
-        year: Number,
-        quantity: Number,
-        imageURL: String
-    
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required : true
+    },
+    description: {
+        type: String,
+    },
+    year: {
+        type: Number
+    },
+    quantity: {
+        type: Number
+    },
+    imageURL: {
+        type: String
+    }
 })
 
 // model and export: 
